1354e91e5626e94b99701a3ed4da6c5160ba77b8
qamba <qambar5464@gmail.com>
2023-09-13 09:58:46 +0300

    a

--- lib/UI_1/asosiy_menu.dart
+++ lib/UI_1/asosiy_menu.dart
@@ -1,3 +1,4 @@
+import 'package:aaa/UI_1/singl_page.dart';
 import 'package:flutter/material.dart';
 
 class Asosiy_Menu extends StatefulWidget {
@@ -8,7 +9,6 @@ class Asosiy_Menu extends StatefulWidget {
 }
 
 class _Asosiy_MenuState extends State<Asosiy_Menu> {
-
   myWidget(String rasmlar) {
     return Padding(
       padding: EdgeInsets.all(10.0),
@@ -20,7 +20,7 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(180),
             image:
-            DecorationImage(image: AssetImage(rasmlar), fit: BoxFit.cover),
+                DecorationImage(image: AssetImage(rasmlar), fit: BoxFit.cover),
             border: Border.all(width: 5, color: Colors.red.shade700),
           ),
         ),
@@ -35,7 +35,7 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
     "assets/8.jpg",
     "assets/1.jpg",
     "assets/5.jpg"
-    "assets/4.jpg"
+        "assets/4.jpg"
   ];
   List<String> avtorrasm = [
     "assets/2.jpg",
@@ -44,18 +44,10 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
     "assets/8.jpg",
     "assets/1.jpg",
     "assets/5.jpg"
-    "assets/4.jpg"
+        "assets/4.jpg"
   ];
 
-  List<String> narxlar = [
-    "200",
-    "400",
-    "500",
-    "600",
-    "700",
-    "800",
-    "1000"
-  ];
+  List<String> narxlar = ["200", "400", "500", "600", "700", "800", "1000"];
 
   List<String> tema = [
     "Yangi video",
@@ -67,15 +59,7 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
     "Videolar"
   ];
 
-  List<String> avtor = [
-    "Ali",
-    "Vali",
-    "G'ani",
-    "Qani",
-    "Jani",
-    "Mani",
-    "Sani"
-  ];
+  List<String> avtor = ["Ali", "Vali", "G'ani", "Qani", "Jani", "Mani", "Sani"];
 
   List<String> yili = [
     "22.01.2023",
@@ -87,7 +71,6 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
     "12.12.2022"
   ];
 
-
   List<String> korishsoni = [
     "24300",
     "404530",
@@ -97,86 +80,124 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
     "834500",
     "145070"
   ];
+  List<String> obunachi = [
+    "24300",
+    "404530",
+    "50430",
+    "63200",
+    "743200",
+    "834500",
+    "145070"
+  ];
 
-  maxsulatlar(String rasm, narx,avtorrasm,Tema,ismavtor,yili,korish) {
+  maxsulatlar(String rasm, narx, avtorrasm, Tema, ismavtor, yili, korish,obunachi) {
     return Expanded(
-
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Stack(
-            children: [
-              Container(
-                height: 280,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+          child: GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>Singl_Page(rasm: rasm, narx: narx,avtorrasm: avtorrasm,Tema: Tema, ismavtor: ismavtor,yili: yili,korish: korish, Obunachi: obunachi,)));
+            },
+            child: Stack(
+              children: [
+               Container(
+                 height: 280,
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(image: AssetImage(rasm),fit: BoxFit.cover),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Container(
-                                color: Colors.blue.shade100,
-                                child: Text("$narx so\'m",style: TextStyle(color: Colors.black,fontSize: 25),),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image: AssetImage(rasm), fit: BoxFit.cover),
                     ),
-                    SizedBox(height: 10,),
-
-                    Row(
+                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Container(
-                          height: 50,
-                            width: 50,
-                           // color: Colors.blue,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              image: DecorationImage(image: AssetImage(avtorrasm),fit: BoxFit.cover),
-                            )
-
-                        ),
-                        SizedBox(width: 15,),
-
-                        Column(
-                         // mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            Text("$Tema",style: TextStyle(color: Colors.black,fontSize: 25),),
-                            SizedBox(height: 2,),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-
-                                Text("$ismavtor",style: TextStyle(color: Colors.black,fontSize: 14),),
-                                SizedBox(width: 20,),
-                                Text("$yili yil",style: TextStyle(color: Colors.black,fontSize: 12),),
-                                SizedBox(width: 15,),
-                                Text("$korish ko'rildi",style: TextStyle(color: Colors.black,fontSize: 12),),
-                              ],
-                            ),
+                            Container(
+                              color: Colors.blue.shade100,
+                              child: Text(
+                                "$narx so\'m",
+                                style:
+                                    TextStyle(color: Colors.black, fontSize: 25),
+                              ),
+                            )
                           ],
-                        ),
+                        )
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                          height: 50,
+                          width: 50,
+                          // color: Colors.blue,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                                image: AssetImage(avtorrasm), fit: BoxFit.cover),
+                          )),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "$Tema",
+                            style: TextStyle(color: Colors.black, fontSize: 25),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                "$ismavtor",
+                                style: TextStyle(
+                                    color: const Color.fromARGB(255, 235, 4, 4),
+                                    fontSize: 14),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                "$yili yil",
+                                style:
+                                    TextStyle(color: Colors.black, fontSize: 12),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                "$korish ko'rildi",
+                                style:
+                                    TextStyle(color: Colors.black, fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-
-
-            ],
+            ),
+        ],
+      ),
           ),
-        ));
+    ));
   }
 
   @override
@@ -345,32 +366,34 @@ class _Asosiy_MenuState extends State<Asosiy_Menu> {
             children: [
               Row(
                 children: [
-                  maxsulatlar(rasmlar[4], narxlar[6],avtorrasm[0],tema[0],avtor[0],yili[0],korishsoni[0]),
-                 // maxsulatlar(rasmlar[5], narxlar[5])
+                  maxsulatlar(rasmlar[4], narxlar[6], avtorrasm[0], tema[0],
+                      avtor[0], yili[0], korishsoni[0],obunachi[0]),
+                  // maxsulatlar(rasmlar[5], narxlar[5])
                 ],
               ),
               Row(
                 children: [
-                 // maxsulatlar(rasmlar[0], narxlar[0],avtorrasm[1],avtor[3]),
-                  maxsulatlar(rasmlar[1], narxlar[1],avtorrasm[2],tema[3],avtor[2],yili[1],korishsoni[3]),
+                  // maxsulatlar(rasmlar[0], narxlar[0],avtorrasm[1],avtor[3]),
+                  maxsulatlar(rasmlar[1], narxlar[1], avtorrasm[2], tema[3],
+                      avtor[2], yili[1], korishsoni[3],obunachi[3]),
                 ],
               ),
               Row(
                 children: [
-                  maxsulatlar(rasmlar[2], narxlar[2],avtorrasm[3],tema[5],avtor[1],yili[3],korishsoni[1]),
-                 // maxsulatlar(rasmlar[3], narxlar[3],avtorrasm[4],avtor[0])
+                  maxsulatlar(rasmlar[2], narxlar[2], avtorrasm[3], tema[5],
+                      avtor[1], yili[3], korishsoni[1],obunachi[2]),
+                  // maxsulatlar(rasmlar[3], narxlar[3],avtorrasm[4],avtor[0])
                 ],
               ),
               Row(
                 children: [
-                  maxsulatlar(rasmlar[4], narxlar[4],avtorrasm[2],tema[2],avtor[4],yili[2],korishsoni[3]),
-                 // maxsulatlar(rasmlar[0], narxlar[5],avtorrasm[3],avtor[5])
+                  maxsulatlar(rasmlar[4], narxlar[4], avtorrasm[2], tema[2],
+                      avtor[4], yili[2], korishsoni[3],obunachi[5]),
+                  // maxsulatlar(rasmlar[0], narxlar[5],avtorrasm[3],avtor[5])
                 ],
               ),
-
             ],
           )
-
         ],
       ),
     );
--- lib/UI_1/singl_page.dart
+++ lib/UI_1/singl_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Singl_Page extends StatefulWidget {
-  const Singl_Page({super.key});
+
+  final String rasm;
+  final String narx;
+  final String avtorrasm;
+  final String Tema;
+  final String ismavtor;
+  final String yili;
+  final String korish;
+  final String Obunachi;
+
+  const Singl_Page({super.key, required this.rasm, required this.narx, required this.avtorrasm, required this.Tema, required this.ismavtor, required this.yili, required this.korish, required this.Obunachi});
+
+
 
   @override
   State<Singl_Page> createState() => _Singl_PageState();
@@ -10,6 +22,215 @@ class Singl_Page extends StatefulWidget {
 class _Singl_PageState extends State<Singl_Page> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ABC"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+          height: 230,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            image: DecorationImage(
+                image: AssetImage(widget.rasm), fit: BoxFit.cover),
+          ),),
+        ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+             // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                Row(
+                  children: [
+
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.Tema,
+                          style: TextStyle(color: Colors.black, fontSize: 25),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+
+                          children: [
+
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              widget.yili+" yil",
+                              style:
+                              TextStyle(color: Colors.black, fontSize: 12),
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Text(
+                              widget.korish+" ko'rildi",
+                              style:
+                              TextStyle(color: Colors.black, fontSize: 12),
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+
+                            Text(
+                             "  Yana  > ",
+                              style:
+                              TextStyle(color: Colors.blue, fontSize: 12,fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(height: 5,color: Colors.grey,),
+
+                SizedBox(
+                  height: 10,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    //crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                       // mainAxisAlignment: MainAxisAlignment.end,
+
+
+
+                        children: [
+                          Container(
+                              height: 50,
+                              width: 50,
+                              // color: Colors.blue,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image: AssetImage(widget.avtorrasm), fit: BoxFit.cover),
+                              )),
+                          SizedBox(
+                            width: 20,
+                          ),
+
+                          Text(
+                            widget.ismavtor,
+                            style: TextStyle(
+                                color: const Color.fromARGB(255, 235, 4, 4),
+                                fontSize: 18,fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 15,
+
+                          ),
+
+                          Text(
+                           widget.Obunachi+ "  Obunachi ",
+                            style:
+                            TextStyle(color: Colors.black, fontSize: 12,fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Row(
+                             // mainAxisAlignment: MainAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                            children: [
+                              Text(" "),
+                              Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(10),
+
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("Obuna Bo'lish",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold),),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(height: 3,color: Colors.grey,),
+
+
+
+
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Chat",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("PlayList",style: TextStyle(color: Colors.white,fontSize: 25),),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Sotib Olish ",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
